5b78192799705a3628d5569e492e402f7e54ecd5
Van Dong <vandong78py@gmail.com>
2026-09-25 18:18:11 +0700

    update code happy birthday

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctPass = "2502";
+const correctPass = "1210";
 let enteredPass = "";
 const dots = document.querySelectorAll(".dot");
 const keys = document.querySelectorAll(".key[data-value]");
